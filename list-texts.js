74c0c522de584d6b436cf398f462dc4ec114b74f
aditya <atulya.bro@gmail.com>
2026-06-15 21:03:03 +0530

    Cleaned up unwanted files from repo

--- list-texts.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('booktok.db');
-const rows = db.prepare('SELECT title, author FROM books WHERE text_content IS NOT NULL').all();
-console.log(`Books with full text (${rows.length}):`);
-rows.forEach(r => console.log(`  ${r.title} by ${r.author}`));
-db.close();
